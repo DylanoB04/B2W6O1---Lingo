f33456096869c20e2dcbdbeff21d34dd11a0b67c
Dylano Brouwer <99064035@mydavinci.nl>
2021-01-24 20:24:03 +0100

    commit 24/1/2021 FINISHED
    Lingo af en getest

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
 //Div
 var DivOne=document.getElementById('DivOne');   var DivTwo=document.getElementById('DivTwo');
 
-//Word List
+//Rows
+var row=0;
+
+//Counter
+var CounterT=document.getElementById('CounterTries');
+var Counter=5;
+CounterT.innerHTML="Tries left: "+Counter;
+console.log(Counter);
+
+//Lingo word
 var RandomWord=words[Math.floor(Math.random() * words.length)];
-var RandomWordSplit=RandomWord.split("");   console.log(RandomWordSplit);
+var WordSplit=RandomWord.split("");   console.log(WordSplit); 
 
 //Buttons
-for(i=1;i<=5;i++){
-    var btn=document.createElement("BUTTON");   btn.id="btn"+i;    btn.innerHTML=i;
-    DivTwo.appendChild(btn);
+var CheckBtn=document.getElementById('CheckBtn');
+
+for(i=0;i<=4;i++){
+    for(j=0;j<=4;j++){
+        var btn=document.createElement("BUTTON");
+        DivTwo.appendChild(btn);
+        btn.id="row"+i+"btn"+j;
+        btn.innerHTML=j;
+        btn.style.backgroundColor="red";
+    };
+};
+
+for(i=0;i<=4;i++){
+    document.getElementById("row"+i+"btn"+0).innerHTML=WordSplit[0];
+    document.getElementById("row"+i+"btn"+0).style.backgroundColor="green";
 }
-btn1.innerHTML=RandomWordSplit[0];
-btn1.style.backgroundColor="green";
+
+CheckBtn.onclick=function(){
+    //User input
+    var WordInput=document.getElementById('WordInput');
+    var UserInput=WordInput.value.split("");    console.log(UserInput);
+    //Empty array for correct letters
+    var CorrectArray=[];
+
+    //Controlling both arrays
+    for(i=0;i<=4;i++){
+        if(WordSplit[i]==UserInput[i]){
+            CorrectArray[i]=true;
+        }else{
+            CorrectArray[i]=false;
+        };
+    };
+
+    for(i=0;i<=4;i++){
+        var BtnInput=document.getElementById("row"+row+"btn"+i);
+        BtnInput.innerHTML=UserInput[i];
+        if(CorrectArray[i]==true){
+            BtnInput.style.backgroundColor="green";
+            WordSplit[i]=null;
+        };
+    };
+
+    for(i=0;i<=4;i++){
+        var BtnInput=document.getElementById("row"+row+"btn"+i);
+        BtnInput.innerHTML=UserInput[i];
+        if(WordSplit.includes(UserInput[i])){       //controleert of de opgegeven waarde aanwezig is in een array
+            BtnInput.style.backgroundColor="yellow";
+            BtnInput.style.borderRadius="100%";
+        };
+    };
+
+    row++
+
+    //Counter 
+    Counter--
+    console.log(Counter);
+    CounterT.innerHTML="Tries left: "+Counter;
+    if(Counter<1){
+        alert("You ran out of tries.");
+        window.location.reload();
+    };
+};
